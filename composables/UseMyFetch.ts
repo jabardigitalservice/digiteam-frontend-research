19bcf909b52a5ef5dc12488a8be2a96d9353889c
sheilaazhar <sheila.azhar98@gmail.com>
2023-03-07 23:12:17 +0700

    set base url v1

--- composables/UseMyFetch.ts
+++ composables/UseMyFetch.ts
@@ -2,7 +2,7 @@ export const useMyFetch = (request: any, opts: any) => {
   const config = useRuntimeConfig();
   const authStore = useAuthStore();
   const requestOptions = {
-    baseURL: config.public.baseURL,
+    baseURL: config.public.baseURL1,
     ...opts,
   };
   if (localStorage.getItem("token")) {
